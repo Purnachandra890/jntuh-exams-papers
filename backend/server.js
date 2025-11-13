d68b2ac869c3bc8414d7cbbc9d811def31e14465
Purnachandra890 <131385647+Purnachandra890@users.noreply.github.com>
2025-11-13 12:57:43 +0530

    small fix

--- backend/server.js
+++ backend/server.js
@@ -52,5 +52,5 @@ mongoose.connect(process.env.MONGO_URI, {/* options */})
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB error:', err));
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
