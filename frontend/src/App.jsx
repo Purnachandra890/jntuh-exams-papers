71a2a34aa41e0d6987c45afbd66a4d22293d1cce
Purnachandra890 <131385647+Purnachandra890@users.noreply.github.com>
2025-11-27 16:28:08 +0530

    small fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Header from './components/Header'
-import InfoBar from './components/InfoBar'
-import Upload from './components/Upload'
-import VerifiedPapers from './components/VerifiedPapers'
-import OnlineUsersBadge from './components/OnlineUsersBadge';
-
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import InfoBar from "./components/InfoBar";
+import Upload from "./components/Upload";
+import VerifiedPapers from "./components/VerifiedPapers";
+import OnlineUsersBadge from "./components/OnlineUsersBadge";
 
 function App() {
+  const API_1 = import.meta.env.VITE_BACKEND_URL_1;
+  const API_2 = import.meta.env.VITE_BACKEND_URL_2;
+  useEffect(() => {
+    const wake = async (url) => {
+      try {
+        // console.log("calling servers..");
+        const res = await fetch(url + "/api/ping");
+        const text=await res.text();
+        console.log(text);
+        // console.log("Response from", url, ":", text);
+      } catch {
+        console.error("Server error:", url, error.message);
+      }
+    };
+
+    wake(API_1);
+    wake(API_2);
+  }, []);
   return (
     <Router>
-      <OnlineUsersBadge/>
+      <OnlineUsersBadge />
       <Routes>
         {/* Home Page Route with Navbar */}
-        <Route path="/" element={
-          <>
-            <Navbar />
-            <Header />
-            <InfoBar />
-          </>
-        } />
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Header />
+              <InfoBar />
+            </>
+          }
+        />
 
         {/* Upload Page Route without Navbar */}
         <Route path="/upload" element={<Upload />} />
@@ -29,7 +49,7 @@ function App() {
         <Route path="/verified-papers" element={<VerifiedPapers />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
